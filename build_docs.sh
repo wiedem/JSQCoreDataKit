967da371174ddf1efbc141c659dacd4e575a5549
Jesse Squires <jesse.d.squires@gmail.com>
2016-01-21 20:24:01 -0800

    auto-update docs

--- build_docs.sh
+++ build_docs.sh
@@ -7,7 +7,7 @@
 git submodule update --remote
 
 jazzy --swift-version 2.1.1 -o ./ \
-      --source-directory JSQCoreDataKit/JSQCoreDataKit/ \
+      --source-directory JSQCoreDataKit/ \
       --readme JSQCoreDataKit/README.md \
       -a 'Jesse Squires' \
       -u 'https://twitter.com/jesse_squires' \
